7715b078b0b0c113aceaf4b18d13e48b044ee2b7
GustavKS <107345877+GustavKS@users.noreply.github.com>
2022-12-03 20:15:36 +0100

    Well

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import ODEs
-
+from scipy.integrate import odeint
 
 # G1 Phase
 IV = {'kd1': 0.1, 'kR': 4.75, 'kAD': 0, 'Am': 1, 'mBR': 1, 'mBA': 1, 'mCLN': 0, 'Cln': 0}
 
-print(ODEs.ODE.V())
+def dmCLNdt(kd1, mCLN):
+  f = -kd1*(mCLN+np.random.randint(0,2))
+  return f
