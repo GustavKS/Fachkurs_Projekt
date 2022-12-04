0662b3e012e0ae6980fc955f8c403eefbf36a696
GustavKS <107345877+GustavKS@users.noreply.github.com>
2022-12-04 12:18:07 +0100

    New thing

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 # G1 Phase
-IV = {'kd1': 0.1, 'kR': 4.75, 'kAD': 0, 'Am': 1, 'mBR': 1, 'mBA': 1, 'mCLN': 0, 'Cln': 0}
+IVG1 = {'kd1': 0.1, 'kR': 4.75, 'kAD': 0, 'Am': 1, 'mBR': 1, 'mBA': 1, 'mCLN': 0, 'Cln': 0}
 
-def dmCLNdt(kd1, mCLN):
-  f = -kd1*(mCLN+np.random.randint(0,2))
-  return f
+# SG2 Phase
+IVSG2 = {'kd1': 0.1, 'kR': 4.75, 'kAD': 0, 'Am': 1, 'mBR': 1, 'mBA': 1, 'mCLN': 0, 'Cln': 0}
+
+
+def dmCLNdt(mCLN, t, kd1=IV['kd1']):
+  dmCLNdt = -kd1*(mCLN+1)
+  return dmCLNdt
+
+t = np.arange(0,351,1)
+sol = odeint(dmCLNdt, 0, t)
+print(sol)
+plt.plot(t, sol+1)
+plt.show()
